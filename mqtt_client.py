72c9b21e17e0e45196ddc8134c0d8db0cafdfec8
floprz <florian.przybylak@koerber.com>
2024-02-14 12:54:55 +0100

    bug fixed:  the mqtt client now reconnects correctly

--- mqtt_client.py
+++ mqtt_client.py
@@ -15,7 +15,7 @@ class MQTTClient:
         self.topics = topics if topics is not None else []
         self.subscribed_topics = set()
         self.processing_chain = None
-
+        self.is_connected = False
         self.client = aiomqtt.Client(hostname=host, port=port, client_id=client_id, username=username, password=password)
         logger.info("Initializing MQTT client...")
         logger.success(f"MQTT client initialized with Host: {host}, Port: {port}, Client ID: {client_id}")
@@ -30,33 +30,30 @@ class MQTTClient:
         return self
 
     async def subscribe_to_topics(self, topics):
-
-        tasks = []
-        interval = 10  # Seconds for retry
-        logger.info(f"Attempting to connect to MQTT broker at {self.hostname}:{self.port}...")
-        while True:
+        interval = 10  # Sekunden für den erneuten Versuch
+        while not self.is_connected:
             try:
                 async with self.client:
                     logger.success("Connected to MQTT broker!")
+                    self.is_connected = True  # Aktualisiere den Verbindungsstatus
+
                     async with self.client.messages() as messages:
-                        for topic in topics:
-                            task = asyncio.create_task(self.client.subscribe(topic))
-                            tasks.append(task)
-                            logger.info(f"Subscribing to topic: {topic}")
-                            self.subscribed_topics.add(topic)
-                        task = asyncio.create_task(self.handle_messages(messages))
-                        tasks.append(task)
+                        tasks = [asyncio.create_task(self.client.subscribe(topic)) for topic in topics]
+                        logger.info(f"Subscribing to topics: {topics}")
+                        self.subscribed_topics.update(topics)
+
+                        tasks.append(asyncio.create_task(self.handle_messages(messages)))
                         await asyncio.gather(*tasks)
+
             except aiomqtt.MqttError as e:
                 logger.danger(f"Failed to connect or lost connection: {e}.")
                 logger.warning(f"Reconnecting in {interval} seconds ...")
+                self.is_connected = False  # Setze den Status zurück, falls ein Fehler auftritt
                 await asyncio.sleep(interval)
             except Exception as e:
                 logger.error(f"An error occurred: {str(e)}")
-            finally:
-                logger.warning("Reconnecting...")
+                self.is_connected = False  # Sicherstellen, dass der Status korrekt zurückgesetzt wird
                 await asyncio.sleep(interval)
-
     async def handle_messages(self, messages):
         async for message in messages:
             asyncio.create_task(self.processing_chain.handle_incoming_message(message, self.client_id))
