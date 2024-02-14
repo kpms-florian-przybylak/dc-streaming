bb592c53b9e013d2b1bdb2c2af0e4350a98d8cbd
floprz <florian.przybylak@koerber.com>
2024-02-14 12:50:40 +0100

    db connection init in rule chain logic

--- rule_chain.py
+++ rule_chain.py
@@ -1,34 +1,48 @@
 import json
 import subprocess
 import os
+
 from helpers.custom_logging_helper import logger
 from typing import Dict, List
-
+import time
 # Ermitteln des Basisverzeichnisses des Projekts
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class RuleChain:
-    def __init__(self, chains_config, targets=None, mqtt_clients=None):
+    def __init__(self, chains_config, targets=None, mqtt_clients=None, db_clients=None):
         self.targets = targets if targets is not None else []
         self.chain = []
         self.mqtt_clients = mqtt_clients
         self.chains_config = chains_config
         self.initialize_chain()
+        self.last_query_time = {}
+        self.db_clients = db_clients
 
     def initialize_chain(self):
         # Initialisieren der Verarbeitungskette basierend auf den übergebenen Schritten
         # und Vorbereiten der Ziele.
         for chain in self.chains_config:
             print("Initializing step", chain)
-
+    def get_last_update_time(self, db_id, query):
+        print("get_last_update_time", query, db_id)
+        # Implementiere eine Methode, um den Zeitstempel der letzten relevanten Datenänderung zu ermitteln.
+        return time.time()
     async def execute_sql_query(self, query, db_id, input_message):
-        print(f"Executing SQL query on {db_id}: {query}")
-        # Hier sollte die tatsächliche SQL-Abfrage ausgeführt und das Ergebnis zurückgegeben werden
-        # Beispiel: Ändern der input_message basierend auf dem Ergebnis der SQL-Abfrage
-        modified_message = input_message  # Modifizieren Sie diese Zeile entsprechend der tatsächlichen Abfrageergebnisse
-        return modified_message
+        # Bestimme, ob die Daten seit der letzten Abfrage geändert wurden
+        last_update_time = self.get_last_update_time(db_id, query)
 
+        if db_id not in self.last_query_time or self.last_query_time[db_id] < last_update_time:
+            # Führe die Abfrage aus, wenn es Änderungen gab oder die Abfrage noch nie ausgeführt wurde
+            db_client = self.db_clients[db_id]
+            result = db_client.execute_query(query)
+            result_list = [dict(row) for row in result]
+            # Aktualisiere den Zeitstempel der letzten erfolgreichen Abfrage
+            self.last_query_time[db_id] = time.time()
+            return result_list  # Oder modifiziere die input_message basierend auf dem Ergebnis
+        else:
+            # Keine Änderungen, kein Bedarf, die Abfrage erneut auszuführen
+            return input_message
     async def execute_python_script(self, script_path, input_message):
         full_script_path = os.path.join(base_dir, 'dc-streaming', 'configs', 'external_scripts', script_path)
         print(f"Executing Python script: {full_script_path}")
