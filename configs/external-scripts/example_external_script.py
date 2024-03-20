90616361284c3535f012c2cc24be00117d0ef52b
floprz <florian.przybylak@koerber.com>
2024-03-20 16:03:15 +0100

    example script added

---
+++ configs/external-scripts/example_external_script.py
@@ -0,0 +1,11 @@
+
+# Einmalige, initiale Ausführung:
+async def initialize(input_message, clients):
+   return
+
+
+# Ausführung bei neuer Nachricht:
+async def process_message(input_message, clients):
+    processed_message = input_message
+
+    return processed_message
